c8525e7f1ff258fc8fe70d410d1467fefce74603
ggramlich26 <14877069+ggramlich26@users.noreply.github.com>
2020-05-30 18:14:35 +0200

    initial commit

---
+++ sloeber.ino.cpp
@@ -0,0 +1,16 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2020-05-30 18:12:14
+
+#include "Arduino.h"
+#include "Arduino.h"
+
+void setup() ;
+void loop() ;
+
+#include "Espressomaschine.ino"
+
+
+#endif
---
+++ spec.d
@@ -0,0 +1,2 @@
+spec.o: \
+ /media/persDat/Mikrocontroller/Sloeber/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
